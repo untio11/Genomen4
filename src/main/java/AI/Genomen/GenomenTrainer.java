9492e1f747df378a32f0a69be7700d069ff60499
Tijn Centen <tijncenten@hotmail.com>
2019-05-17 17:05:49 +0200

    Update GenomenTrainer for new BiAIGameTrainer class

--- src/main/java/AI/Genomen/GenomenTrainer.java
+++ src/main/java/AI/Genomen/GenomenTrainer.java
@@ -1,7 +1,7 @@
 package AI.Genomen;
 
 import AI.Genomen.Player.AIGenomenPlayer;
-import AI.Trainer.AIGameTrainer;
+import AI.Trainer.BiAIGameTrainer;
 import GameState.World;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import util.Pair;
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 // TODO: Switch out the World class with the Game class that can run the game headless
-public class GenomenTrainer extends AIGameTrainer<AIGenomenPlayer, World> {
+public class GenomenTrainer extends BiAIGameTrainer<AIGenomenPlayer, AIGenomenPlayer, World> {
 
     public GenomenTrainer(int nPlayers, int iterations) {
         super(nPlayers, iterations);
@@ -22,27 +22,37 @@ public class GenomenTrainer extends AIGameTrainer<AIGenomenPlayer, World> {
     }
 
     @Override
-    protected AIGenomenPlayer createPlayer() {
+    protected void playGame(World game) {
+
+    }
+
+    @Override
+    protected AIGenomenPlayer createPlayer1() {
         return null;
     }
 
     @Override
-    protected AIGenomenPlayer createPlayer(Map<String, INDArray> paramTable) {
+    protected AIGenomenPlayer createPlayer1(Map<String, INDArray> paramTable) {
         return null;
     }
 
     @Override
-    protected List<Pair<AIGenomenPlayer, AIGenomenPlayer>> createCompetition(List<AIGenomenPlayer> players) {
+    protected AIGenomenPlayer createPlayer2() {
+        return null;
+    }
+
+    @Override
+    protected AIGenomenPlayer createPlayer2(Map<String, INDArray> paramTable) {
+        return null;
+    }
+
+    @Override
+    protected List<Pair<AIGenomenPlayer, AIGenomenPlayer>> createCompetition(List<AIGenomenPlayer> players1, List<AIGenomenPlayer> players2) {
         return null;
     }
 
     @Override
     protected World createGame(Pair<AIGenomenPlayer, AIGenomenPlayer> players) {
         return null;
-    }
-
-    @Override
-    protected void playGame(World game) {
-
     }
 }
