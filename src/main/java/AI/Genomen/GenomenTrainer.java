6e963d3df92e2cfe9735d3c5cc96f4fcfdaca5b0
Tijn Centen <tijncenten@hotmail.com>
2019-05-18 11:54:27 +0200

    Add the creation of players and competition to the GenomenTrainer Creation of the game environment is still to be done

--- src/main/java/AI/Genomen/GenomenTrainer.java
+++ src/main/java/AI/Genomen/GenomenTrainer.java
@@ -1,11 +1,13 @@
 package AI.Genomen;
 
+import AI.ConnectFour.Player.AIConnectFourPlayer;
 import AI.Genomen.Player.AIGenomenPlayer;
 import AI.Trainer.BiAIGameTrainer;
 import GameState.World;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import util.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,41 +20,59 @@ public class GenomenTrainer extends BiAIGameTrainer<AIGenomenPlayer, AIGenomenPl
 
     @Override
     protected String getName() {
+        return "genomen";
+    }
+
+    @Override
+    protected AIGenomenPlayer createPlayer1() {
+        return new AIGenomenPlayer();
+    }
+
+    @Override
+    protected AIGenomenPlayer createPlayer1(Map<String, INDArray> paramTable) {
+        AIGenomenPlayer player = new AIGenomenPlayer();
+        player.init();
+        player.getNetwork().setParamTable(paramTable);
+        return player;
+    }
+
+    @Override
+    protected AIGenomenPlayer createPlayer2() {
+        return new AIGenomenPlayer();
+    }
+
+    @Override
+    protected AIGenomenPlayer createPlayer2(Map<String, INDArray> paramTable) {
+        AIGenomenPlayer player = new AIGenomenPlayer();
+        player.init();
+        player.getNetwork().setParamTable(paramTable);
+        return player;
+    }
+
+    @Override
+    protected List<Pair<AIGenomenPlayer, AIGenomenPlayer>> createCompetition(List<AIGenomenPlayer> players1, List<AIGenomenPlayer> players2) {
+        List<Pair<AIGenomenPlayer, AIGenomenPlayer>> competition = new ArrayList<>();
+
+        // Create a bipartite graph as the competition
+        for (AIGenomenPlayer player1 : players1) {
+            for (AIGenomenPlayer player2 : players2) {
+                competition.add(new Pair<>(player1, player2));
+            }
+        }
+
+        return competition;
+    }
+
+    @Override
+    protected World createGame(Pair<AIGenomenPlayer, AIGenomenPlayer> players) {
+        // TODO: Over here we should be able to initialize a game that is headless and assign the players to it
+        // TODO: We should also be able to add listeners to the game end state
         return null;
     }
 
     @Override
     protected void playGame(World game) {
-
-    }
-
-    @Override
-    protected AIGenomenPlayer createPlayer1() {
-        return null;
-    }
-
-    @Override
-    protected AIGenomenPlayer createPlayer1(Map<String, INDArray> paramTable) {
-        return null;
-    }
-
-    @Override
-    protected AIGenomenPlayer createPlayer2() {
-        return null;
-    }
-
-    @Override
-    protected AIGenomenPlayer createPlayer2(Map<String, INDArray> paramTable) {
-        return null;
-    }
-
-    @Override
-    protected List<Pair<AIGenomenPlayer, AIGenomenPlayer>> createCompetition(List<AIGenomenPlayer> players1, List<AIGenomenPlayer> players2) {
-        return null;
-    }
-
-    @Override
-    protected World createGame(Pair<AIGenomenPlayer, AIGenomenPlayer> players) {
-        return null;
+        // TODO: Start the Genomen game headless and play it
+        // TODO: Retrieve the results and store them for the players
     }
 }
