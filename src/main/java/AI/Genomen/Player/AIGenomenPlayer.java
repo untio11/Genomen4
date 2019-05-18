807eb7e425cb9b9d037456ae583c436163c39a27
Tijn Centen <tijncenten@hotmail.com>
2019-05-18 11:53:10 +0200

    Adapt the AIGenomenPlayer so it can be used in the GenomenTrainer

--- src/main/java/AI/Genomen/Player/AIGenomenPlayer.java
+++ src/main/java/AI/Genomen/Player/AIGenomenPlayer.java
@@ -1,15 +1,32 @@
 package AI.Genomen.Player;
 
+import AI.ConnectFour.PlayConnectFour;
 import AI.Trainer.TrainerAIPlayer;
 import GameState.Entities.Actor;
 import Graphics.Models.TexturedModel;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
 import org.joml.Vector3f;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.File;
 import java.io.IOException;
 
 public class AIGenomenPlayer extends Actor implements TrainerAIPlayer {
+
+    protected MultiLayerNetwork net;
+
+    public AIGenomenPlayer() {
+        super(null, null, 0, 0, 0, 1f);
+    }
+
     /**
      * Initialize a player with the appropriate properties
      *
@@ -24,18 +41,50 @@ public class AIGenomenPlayer extends Actor implements TrainerAIPlayer {
         super(model, position, rotX, rotY, rotZ, scale);
     }
 
+    public void init() {
+        if (this.net == null) {
+            this.createNetwork();
+        }
+    }
+
+    protected void createNetwork() {
+        // TODO: Improve dummy network
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .updater(new Sgd(0.1))
+                .biasInit(0)
+                .miniBatch(false)
+                .list()
+                .layer(new DenseLayer.Builder()
+                        .nIn(PlayConnectFour.SIZE*PlayConnectFour.SIZE)
+                        .nOut(PlayConnectFour.SIZE*PlayConnectFour.SIZE)
+                        .activation(Activation.TANH)
+                        .weightInit(new UniformDistribution(-1, 1))
+                        .build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.L2)
+                        .nOut(PlayConnectFour.SIZE*PlayConnectFour.SIZE)
+                        .activation(Activation.SOFTMAX)
+//                        .weightInit(WeightInit.DISTRIBUTION)
+                        .weightInit(new UniformDistribution(0, 1))
+                        .build())
+                .build();
+
+        net = new MultiLayerNetwork(conf);
+        net.init();
+    }
+
     @Override
     public MultiLayerNetwork getNetwork() {
-        return null;
+        return this.net;
     }
 
     @Override
     public void saveNetwork(File f) throws IOException {
-
+        ModelSerializer.writeModel(this.net, f, false);
     }
 
     @Override
     public MultiLayerNetwork loadNetwork(File f) throws IOException {
-        return null;
+        this.net = ModelSerializer.restoreMultiLayerNetwork(f, false);
+        return this.net;
     }
 }
