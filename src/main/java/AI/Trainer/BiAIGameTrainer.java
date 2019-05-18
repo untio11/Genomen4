b8ba3f156613723acddfe59454c0e3804d41cc73
Tijn Centen <tijncenten@hotmail.com>
2019-05-18 11:35:12 +0200

    Add BiAIGameTrainer implementation of abstract BaseAIGameTrainer methods

--- src/main/java/AI/Trainer/BiAIGameTrainer.java
+++ src/main/java/AI/Trainer/BiAIGameTrainer.java
@@ -3,10 +3,7 @@ package AI.Trainer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import util.Pair;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class BiAIGameTrainer<A extends TrainerAIPlayer, B extends TrainerAIPlayer, G> extends BaseAIGameTrainer<G> {
 
@@ -61,22 +58,59 @@ public abstract class BiAIGameTrainer<A extends TrainerAIPlayer, B extends Train
 
     @Override
     protected void playCompetition() {
-
+        for (Pair<A, B> players : competition) {
+            G game = this.createGame(players);
+            this.playGame(game);
+        }
     }
 
     @Override
     protected void evaluateCompetition() {
+        // Get the best player from group 1
+        LinkedHashMap<A, Integer> sortedPlayers1 = evaluatePlayers(playerScores1);
+        Map.Entry<A, Integer> bestPlayer1Entry = sortedPlayers1.entrySet().iterator().next();
 
+        // Get the best player from group 2
+        LinkedHashMap<B, Integer> sortedPlayers2 = evaluatePlayers(playerScores2);
+        Map.Entry<B, Integer> bestPlayer2Entry = sortedPlayers2.entrySet().iterator().next();
+
+        Date date = new Date();
+
+        // Save the best player from group 1
+        String fileName1 = date.getTime() + "-" + this.getName() + "-1" + "-" + bestPlayer1Entry.getValue();
+        this.savePlayer(bestPlayer1Entry.getKey(), fileName1);
+
+        // Save the best player from group 2
+        String fileName2 = date.getTime() + "-" + this.getName() + "-2" + "-" + bestPlayer2Entry.getValue();
+        this.savePlayer(bestPlayer2Entry.getKey(), fileName2);
+
+        System.out.println("Best Scores: \t" + bestPlayer1Entry.getValue() + "\t | \t" + bestPlayer2Entry.getValue());
     }
 
     @Override
     protected void performGeneticEvolution() {
+        // Genetically evolve player group 1
+        LinkedHashMap<A, Integer> sortedPlayers1 = evaluatePlayers(playerScores1);
+        players1 = geneticAlgorithm1.performPlayerEvolution(sortedPlayers1);
 
+        // Genetically evolve player group 2
+        LinkedHashMap<B, Integer> sortedPlayers2 = evaluatePlayers(playerScores2);
+        players2 = geneticAlgorithm2.performPlayerEvolution(sortedPlayers2);
     }
 
     @Override
     protected void resetGeneticAlgorithm() {
+        // Reset player group 1
+        playerScores1 = new HashMap<>();
+        for (A player : players1) {
+            playerScores1.put(player, 0);
+        }
 
+        // Reset player group 2
+        playerScores2 = new HashMap<>();
+        for (B player : players2) {
+            playerScores2.put(player, 0);
+        }
     }
 
 
@@ -94,6 +128,24 @@ public abstract class BiAIGameTrainer<A extends TrainerAIPlayer, B extends Train
             players2.add(player);
             playerScores2.put(player, 0);
         }
+    }
+
+    public void setResults1(A player, int score) {
+        int oldScore = playerScores1.get(player);
+        playerScores1.put(player, oldScore + score);
+    }
+
+    public void setResults2(B player, int score) {
+        int oldScore = playerScores2.get(player);
+        playerScores2.put(player, oldScore + score);
+    }
+
+    public LinkedHashMap<A, Integer> getScoredPlayers1() {
+        return evaluatePlayers(playerScores1);
+    }
+
+    public LinkedHashMap<B, Integer> getScoredPlayers2() {
+        return evaluatePlayers(playerScores2);
     }
 
     protected abstract A createPlayer1();
