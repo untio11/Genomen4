522b4d4a2fc8a880c6a5f63384cf50e3ce2c402d
Julian Smits <juliansmits@hotmail.com>
2019-05-31 11:13:42 +0200

    Implement copy parents from old generation + fix fow low amount of players

--- src/main/java/AI/Trainer/GeneticAlgorithm.java
+++ src/main/java/AI/Trainer/GeneticAlgorithm.java
@@ -9,8 +9,10 @@ public class GeneticAlgorithm<A extends TrainerAIPlayer> {
 
     private BaseAIGameTrainer<?>.AIPlayerBuilder<A> playerBuilder;
 
-    private float mutateProbability = 0.05f;
-    private float mutatePercentage = 0.1f;
+    private float mutateProbability = 0.005f;
+    private float mutatePercentage = 0.0005f;
+
+    private float copyParentPercentage = 0.15f;
 
     public GeneticAlgorithm(BaseAIGameTrainer<?>.AIPlayerBuilder<A> playerBuilder) {
         this.playerBuilder = playerBuilder;
@@ -23,8 +25,17 @@ public class GeneticAlgorithm<A extends TrainerAIPlayer> {
             sum += value;
         }
         List<A> newPlayers = new ArrayList<>();
+
+        // Copy parents from old generation
+        int numberOfParents = (int) Math.ceil(sortedPlayers.size() * copyParentPercentage);
+        for (int i = 0; i < numberOfParents; i++) {
+            A parent = this.selectParent(accumulatedPlayers, sum);
+            A copyParent = playerBuilder.createPlayer(parent.getNetwork().paramTable());
+            newPlayers.add(copyParent);
+        }
+
         // Create new players by generating from previous iteration for every new player
-        for (int i = 0; i < sortedPlayers.size(); i++) {
+        for (int i = newPlayers.size(); i < sortedPlayers.size(); i++) {
             // -> Selection
             // Select two random parents for the child with a higher probability of well performing players
             A parent1 = this.selectParent(accumulatedPlayers, sum);
@@ -66,7 +77,7 @@ public class GeneticAlgorithm<A extends TrainerAIPlayer> {
 
     private A selectParent(LinkedHashMap<A, Integer> players, int sum) {
         Random r = new Random();
-        int v = r.nextInt(sum);
+        int v = r.nextInt(Math.max(1, sum));
         Iterator<Map.Entry<A, Integer>> iterator = players.entrySet().iterator();
         A parent = null;
         while(v >= 0 && iterator.hasNext()) {
