9dfda070abb98c94e7f105bb7f2f8fcca049fb1c
Tijn Centen <tijncenten@hotmail.com>
2019-05-31 11:22:52 +0200

    Add duration of genetic algorithm to prints

--- src/main/java/AI/Genomen/GenomenTrainer.java
+++ src/main/java/AI/Genomen/GenomenTrainer.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class GenomenTrainer extends BiAIGameTrainer<AIGenomenPlayer, AIGenomenPlayer, GameContainer> {
 
@@ -32,8 +33,16 @@ public class GenomenTrainer extends BiAIGameTrainer<AIGenomenPlayer, AIGenomenPl
         int players = 2;
         GenomenTrainer trainer = new GenomenTrainer(players, 1);
 
+        long startTime = System.nanoTime();
+
         trainer.init();
         trainer.runGeneticAlgorithm();
+
+        long endTime = System.nanoTime();
+        long durationNano = endTime - startTime;
+        long durationSecs = TimeUnit.NANOSECONDS.toSeconds(durationNano);
+
+        System.out.println("Genetic algorithm duration: " +  durationSecs + " seconds");
 
         LinkedHashMap<AIGenomenPlayer, Integer> sortedPlayers = trainer.getScoredPlayers2();
 
