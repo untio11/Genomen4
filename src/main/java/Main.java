9e312448e0570692372017db9e642a0e864c0662
Tijn Centen <tijncenten@hotmail.com>
2019-06-05 15:01:35 +0200

    Remove unnecessary imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,3 @@
-import GameState.Entities.Camera;
-import GameState.Entities.Actor;
-import GameState.MapGenerator;
-import GameState.TileType;
-import Graphics.Models.RawModel;
-import Graphics.Models.TexturedModel;
-import Graphics.RenderEngine.Loader;
-import Graphics.RenderEngine.MasterRenderer;
-import Graphics.RenderEngine.OBJLoader;
-import Graphics.Terrains.Terrain;
-import Graphics.Textures.ModelTexture;
-import Graphics.Textures.TerrainTexture;
-import org.joml.Vector3f;
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-import java.util.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
 import GameState.World;
 import Graphics.WindowManager;
 
