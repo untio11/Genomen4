5f7d8cbef186be867558022f92ea14dc6f94ca28
Thanh-Dat Nguyen <thanh-dat@live.fr>
2019-06-07 09:41:50 +0200

    remove duplicate code

--- src/main/java/Engine/GameContainerSwing.java
+++ src/main/java/Engine/GameContainerSwing.java
@@ -3,38 +3,11 @@ package Engine;
 import Engine.Controller.Controller;
 import Engine.Controller.KeyController;
 import GameState.World;
-import Graphics.RenderEngine.AbstractRenderer;
-import Graphics.RenderEngine.Scene;
 
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
-public class GameContainerSwing implements Runnable, AbstractGameContainer {
-
-    private static final double ROUND_TIME = 60;
-    private final int FPS = 60;
-    private final double UPDATE_CAP = 1.0 / FPS;
-    private final double cryInterval = 7;
-
-    private boolean renderWindow;
-    private int pixelWidth, pixelHeight;
-    private float scale = 0.5f;
-    private Scene scene;
-
-    private double roundTime;
-    private boolean fatherWin;
-
-    private Thread thread = new Thread(this);
-
-    private Window window;
-    private AbstractRenderer renderer; // TODO: Add extra layer above MasterRenderer (AbstractRenderer?) to cover normal rasterizition shading and raytracing
-    private Controller fatherController, kidnapperController;
-
-    ArrayList<SoundClip> clips;
-    SoundClip music;
-
-    private World world;
-    private int maxDistance;
+public class GameContainerSwing extends AbstractGameContainer {
 
     private boolean humanPlayer = false;
 
@@ -118,17 +91,6 @@ public class GameContainerSwing implements Runnable, AbstractGameContainer {
         }
     }
 
-    /**
-     * Initialise game and run.
-     */
-    public void start() {
-        if (kidnapperController != null || fatherController != null) {     //if all the controllers have been initialized
-            thread.run();
-        } else {
-            System.err.println("Please define controllers");
-        }
-    }
-
     public void close() {}
 
     /**
@@ -143,121 +105,6 @@ public class GameContainerSwing implements Runnable, AbstractGameContainer {
         }
     }
 
-    public void headless() {
-        double passedTime = 0;
-        double cryTimer = cryInterval;
-        boolean running = true;
-        double roundTime= ROUND_TIME;
-
-        while (running){
-            updateActor();
-            roundTime -= UPDATE_CAP;
-            passedTime += UPDATE_CAP;
-
-            cryTimer -= passedTime;
-            if (cryTimer < 0) {
-                World.getInstance().getKidnapper().receiveScream();
-                World.getInstance().getFather().receiveScream();
-                cryTimer = cryInterval;
-            }
-
-            if (world.isPlayerCollision()) {
-                fatherWin = true;
-                running = false;
-                break;
-            } else if (roundTime < 0) {
-                fatherWin = false;
-                running = false;
-                break;
-            }
-        }
-        this.roundTime = roundTime;
-    }
-
-
-    public void windowed(){
-        boolean render;
-        double firstTime;
-        double lastTime = System.nanoTime() / 1e9d;
-        double passedTime;
-        double unprocessedTime = 0;
-        double frameTime = 0;
-        int frames = 0;
-        int fps = 0;
-        double cryTimer = cryInterval;
-        int cryNumber = 0;
-        boolean running = true;
-        double roundTime= ROUND_TIME;
-
-        music.loop();
-        while (running) {
-            render = false;
-            firstTime = System.nanoTime() / 1e9d;
-            passedTime = firstTime - lastTime;
-            lastTime = firstTime;
-            unprocessedTime += passedTime;
-            frameTime += passedTime;
-            roundTime -= passedTime;
-            
-            cryTimer -= passedTime;
-            if (cryTimer < 0) {
-                World.getInstance().getKidnapper().receiveScream();
-                World.getInstance().getFather().receiveScream();
-                cryTimer = cryInterval;
-                clips.get(cryNumber).play();
-                cryNumber = (cryNumber + 1) % clips.size();
-            }
-
-            //in case the game freezes, the while loop tries to catch up by updating faster
-            while (unprocessedTime >= UPDATE_CAP) {
-
-                render = true;
-                unprocessedTime -= UPDATE_CAP;
-
-                //update game
-                updateActor();
-
-                if (frameTime >= 1.0) {
-                    frameTime = 0;
-                    fps = frames;
-                    System.out.println(fps + "fps <=> " + (float) 1000/frames + "ms/frame");
-                    frames = 0;
-                }
-            }
-
-            if (world.isPlayerCollision()) {
-                if (this.renderWindow) {
-                    window.close();
-                }
-                fatherWin = true;
-                running = false;
-                break;
-            } else if (roundTime < 0) {
-                if (this.renderWindow) {
-                    window.close();
-                }
-                fatherWin = false;
-                running = false;
-                break;
-            }
-
-            if (render) {
-                finalRender();
-
-                frames++;
-            } else {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        music.stop();
-        close();
-        this.roundTime = roundTime;
-    }
-
     public void finalRender() {
         renderer.render(scene);  //render game
     }
@@ -267,21 +114,5 @@ public class GameContainerSwing implements Runnable, AbstractGameContainer {
         kidnapperController.update(UPDATE_CAP);
     }
 
-    public double getRemainingTime() {
-        return roundTime;
-    }
 
-    public boolean isFatherWin() {
-        return fatherWin;
-    }
-
-    public int getMaxDistance() {
-        return maxDistance;
-    }
-
-    public static double getRoundTime() { return ROUND_TIME; }
-
-    public Controller getFatherController() { return fatherController; }
-
-    public Controller getKidnapperController() { return kidnapperController; }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import Engine.AbstractGameContainer;
 import Engine.GameContainerSwing;
 import GameState.MapConfigurations;
 import GameState.World;
-import Graphics.GameContainerGL;
 
 import java.io.File;
 import java.io.IOException;
